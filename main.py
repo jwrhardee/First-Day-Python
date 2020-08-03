906450c8ad1cda431d9599a9666237df75af48ca
jesseHardee <jwrhardee@gmail.com>
2020-08-03 20:52:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# print("hello world")
+
+# # VARIABLE - a way to store a single piece of data 
+# # nameofvariable = value
+# # ex score and health in a game 
+# score = 0
+# name = "spongebob"
+# health = 100.405679
+
+# print (name)
+
+# #conditionals - if/else statements used for descion
+
+# if score  == 5: 
+#   print("your score is 0")
+# else:
+#   print("your score is 0")
+
+# num = 30  
+
+# #create a if/else statment that will print
+# #wether num is less than 30
+
+# if num == 30: 
+#   print ("greater than 30")
+# else:
+#   print("less than 30") 
+
+
+
+#  # Loops - a reapet code given amount of times or until a conditoin 
+# for i in range(5):
+#   print("smelly cat")
+
+# count = 0
+# while (count < 9):
+#   print ("the count is " + str(count ))#count = count + 1
+#   count += 1
+
+name = input("what is your name?")
+print("Hello " +name )
+
+avengers = ("black widow ","iron man")
+
+avengers_count = 4
+
+#write a program thats asks for name age and show them thier info agian
+
+
+fullName = input ("what is your first and last name")
+age = input ("how old are you ")
+activity = input ("what is your favorite activity")
+# ask user favritoe activitey 
+# print out users favorite activity in a setence. 
+print ("your name is "+ fullName)
+print ("your favorite activity is "+ activity)
+print ("your age is " + age)
+num1 = int(input("put a number down"))
+num2 = int(input("put a new number"))
+print("the sum is :{}" .format (num1+num2))
